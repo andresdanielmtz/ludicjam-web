ec152ec68cb002cdd055092351a07dfe905b8b9d
BrunoGaxiola <A01253874tec.mx>
2024-09-10 18:23:44 -0700

    feat: both footer and phases implemented.

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import "./App.css";
 import Map from "./components/Map/Map";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+import Phases from "./components/Phases/Phases";
 function App() {
   return (
     <>
       <div id="root">
         <Header />
+        <Phases />
         <div className="mapComponent">
           <Map />
         </div>

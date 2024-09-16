45f83eb429400e8150c7c0e6ce3c218c615c11db
DanFC <danielclausen12@gmail.com>
2024-09-16 15:31:41 -0700

    background video added

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,5 @@
 import LudicWebLandingVideo from "./../../assets/LudicWebLandingVideo.mp4";
+import "./Header.css";
 
 const Header = () => {
   return (
@@ -6,9 +7,9 @@ const Header = () => {
       <video className="background-video" autoPlay loop muted>
         <source src={LudicWebLandingVideo} type="video/mp4" />
       </video>
-
+      <div className="overlay"></div>
       <div className="header-text">
-        <h1>Bienvenido al Game Jam más grande de Hermosillo.</h1>
+        <h1>Bienvenido al Game Jam más grande de Hermosillo</h1>
       </div>
     </div>
   );
